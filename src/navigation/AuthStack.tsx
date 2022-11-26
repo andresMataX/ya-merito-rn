ec292c7b5b480c980e7a32dbd4d35de5e2a52a45
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-25 21:58:06 -0600

    Auth folder

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Login } from '../screens/Auth/Login';
+import { SignUp } from '../screens/Auth/SignUp';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -8,6 +9,7 @@ export const AuthStak = () => {
   return (
     <Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
       <Screen name="Login" component={Login} />
+      <Screen name="SignUp" component={SignUp} />
     </Navigator>
   );
 }
--- src/screens/Auth/Login.tsx
+++ src/screens/Auth/Login.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const Login = () => {
 
+  const inset = useSafeAreaInsets();
+
   return (
-    <View>
+    <View style={{ top: inset.top }}>
       <Text style={{ color: 'black' }}>Login</Text>
     </View>
   )
