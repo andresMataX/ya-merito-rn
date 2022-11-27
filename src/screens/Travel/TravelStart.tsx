158b37bc91a6f880cf89f8f8ebd2bb4e836446f1
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-11-26 21:07:44 -0600

    Estilos de contenedor de mapa

--- src/screens/Travel/TravelStart.tsx
+++ src/screens/Travel/TravelStart.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import { DrawerScreenProps } from '@react-navigation/drawer';
 import Icon from 'react-native-vector-icons/Ionicons';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
@@ -25,20 +25,35 @@ export const TravelStart = ({ navigation }: Props) => {
   }, [])
 
   return (
-    <View>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        style={{
-          width: '100%',
-          height: '100%'
-        }}
-      />
+    <View style={styles.mainContainer}>
+      <View style={styles.mapContainer}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    alignItems: 'center',
+    flex: 1
+  },
+  mapContainer: {
+    height: '48%',
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 2,
+  }
+});
